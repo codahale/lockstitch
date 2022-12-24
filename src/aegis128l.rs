80abd77dadd31b2c723403200112bec7a0e2a728
Coda Hale <coda.hale@gmail.com>
2022-12-24 14:54:15 -0700

    test: add KAT back

--- src/aegis128l.rs
+++ src/aegis128l.rs
@@ -248,6 +248,33 @@ mod tests {
         );
     }
 
+    #[test]
+    fn test_aegis_in_place() {
+        let m = b"Ladies and Gentlemen of the class of '99: If I could offer you only one tip for the future, sunscreen would be it.";
+        let ad = b"Comment numero un";
+        let key = b"YELLOW SUBMARINE";
+        let nonce = [0u8; 16];
+
+        let mut mc = m.to_vec();
+        let tag = encrypt(key, &nonce, &mut mc, ad);
+        let expected_mc = [
+            137, 147, 98, 134, 30, 108, 100, 90, 185, 139, 110, 255, 169, 201, 98, 232, 138, 159,
+            166, 71, 169, 80, 96, 205, 2, 109, 22, 101, 71, 138, 231, 79, 130, 148, 159, 175, 131,
+            148, 166, 200, 180, 159, 139, 138, 80, 104, 188, 50, 89, 53, 204, 111, 12, 212, 196,
+            143, 98, 25, 129, 118, 132, 115, 95, 13, 232, 167, 13, 59, 19, 143, 58, 59, 42, 206,
+            238, 139, 2, 251, 194, 222, 185, 59, 143, 116, 231, 175, 233, 67, 229, 11, 219, 127,
+            160, 215, 89, 217, 109, 89, 76, 225, 102, 118, 69, 94, 252, 2, 69, 205, 251, 65, 159,
+            177, 3, 101,
+        ];
+        let expected_tag = [16, 244, 133, 167, 76, 40, 56, 136, 6, 235, 61, 139, 252, 7, 57, 150];
+        assert_eq!(mc, expected_mc);
+        assert_eq!(tag, expected_tag);
+
+        let tag = decrypt(key, &nonce, &mut mc, ad);
+        assert_eq!(mc, m);
+        assert_eq!(tag, expected_tag);
+    }
+
     proptest! {
         #[test]
         fn decrypting_from_rust_aegis(

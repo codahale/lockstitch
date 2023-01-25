52691a98d771bba6acbcaeb712f719fa6008b465
Coda Hale <coda.hale@gmail.com>
2023-01-24 17:00:08 -0700

    fix: remove old reference to neon feature

--- xtask/src/xtask.rs
+++ xtask/src/xtask.rs
@@ -18,10 +18,6 @@ enum Command {
 
     // Run benchmarks.
     Bench {
-        /// Enable NEON optimization.
-        #[clap(long, default_value = "false")]
-        neon: bool,
-
         /// Additional arguments for criterion.
         #[clap(action(ArgAction::Append), allow_hyphen_values = true)]
         args: Vec<String>,
@@ -57,7 +53,7 @@ fn main() -> Result<()> {
 
     match xtask.cmd.unwrap_or(Command::CI) {
         Command::CI => ci(&sh),
-        Command::Bench { neon, args } => bench(&sh, neon, args),
+        Command::Bench { args } => bench(&sh, args),
         Command::Cloud { cmd } => match cmd {
             CloudCommand::Create => cloud_create(&sh),
             CloudCommand::Setup => cloud_setup(&sh),
@@ -78,15 +74,9 @@ fn ci(sh: &Shell) -> Result<()> {
     Ok(())
 }
 
-fn bench(sh: &Shell, neon: bool, args: Vec<String>) -> Result<()> {
+fn bench(sh: &Shell, args: Vec<String>) -> Result<()> {
     let args = args.join(" ");
-    if neon {
-        cmd!(sh, "cargo criterion --features=neon {args}")
-            .env("RUSTFLAGS", "-C target-cpu=native")
-            .run()?;
-    } else {
-        cmd!(sh, "cargo criterion {args}").env("RUSTFLAGS", "-C target-cpu=native").run()?;
-    }
+    cmd!(sh, "cargo criterion {args}").env("RUSTFLAGS", "-C target-cpu=native").run()?;
 
     Ok(())
 }

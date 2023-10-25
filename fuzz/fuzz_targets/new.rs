cde1c8abc32151861b4babe029793d3fdb132404
Coda Hale <coda.hale@gmail.com>
2023-10-25 15:15:11 -0600

    refactor(fuzz): simplify inputs for new

--- fuzz/fuzz_targets/new.rs
+++ fuzz/fuzz_targets/new.rs
@@ -4,8 +4,6 @@ use std::str;
 use libfuzzer_sys::fuzz_target;
 use lockstitch::Protocol;
 
-fuzz_target!(|data: &[u8]| {
-    if let Ok(utf8) = str::from_utf8(data) {
-        let _ = Protocol::new(utf8);
-    }
+fuzz_target!(|data: &str| {
+    let _ = Protocol::new(data);
 });

c5e0162b2a918660b44bb2e7d053b9f75cff8216
Coda Hale <coda.hale@gmail.com>
2023-10-30 20:42:24 -0600

    refactor: pull fns out of the proptest macro

--- tests/writers_test.rs
+++ tests/writers_test.rs
@@ -8,61 +8,85 @@ fn writes() -> impl Strategy<Value = Vec<Vec<u8>>> {
     vec(vec(any::<u8>(), 0..200), 0..100)
 }
 
+fn mix_as_slice(writes: &[Vec<u8>]) -> [u8; 16] {
+    let mut protocol = Protocol::new("com.example.test");
+    let combined = writes.iter().flatten().copied().collect::<Vec<u8>>();
+    protocol.mix(&combined);
+    protocol.derive_array()
+}
+
+fn mix_as_writer(writes: &[Vec<u8>]) -> [u8; 16] {
+    let protocol = Protocol::new("com.example.test");
+    let mut writer = protocol.mix_writer(io::sink());
+    for buf in writes.iter() {
+        writer.write_all(buf).expect("sink writes should be infallible");
+    }
+    let (mut protocol, _) = writer.into_inner();
+    protocol.derive_array()
+}
+
+fn encrypt_as_slice(writes: &[Vec<u8>]) -> (Vec<u8>, [u8; 16]) {
+    let mut protocol = Protocol::new("com.example.test");
+    let mut plaintext = writes.iter().flatten().copied().collect::<Vec<u8>>();
+    protocol.encrypt(&mut plaintext);
+    (plaintext, protocol.derive_array())
+}
+
+fn encrypt_as_writer(writes: &[Vec<u8>]) -> (Vec<u8>, [u8; 16]) {
+    let protocol = Protocol::new("com.example.test");
+    let mut ciphertext = Vec::new();
+    let mut writer = protocol.encrypt_writer(&mut ciphertext);
+    for buf in writes.iter() {
+        writer.write_all(buf).expect("vec writes should be infallible");
+    }
+    let (mut protocol, _) = writer.into_inner().expect("vec writes should be infallible");
+    (ciphertext, protocol.derive_array())
+}
+
+fn decrypt_as_slice(writes: &[Vec<u8>]) -> (Vec<u8>, [u8; 16]) {
+    let mut protocol = Protocol::new("com.example.test");
+    let mut ciphertext = writes.iter().flatten().copied().collect::<Vec<u8>>();
+    protocol.decrypt(&mut ciphertext);
+    (ciphertext, protocol.derive_array())
+}
+
+fn decrypt_as_writer(writes: &[Vec<u8>]) -> (Vec<u8>, [u8; 16]) {
+    let protocol = Protocol::new("com.example.test");
+    let mut plaintext = Vec::new();
+    let mut writer = protocol.decrypt_writer(&mut plaintext);
+    for buf in writes.iter() {
+        writer.write_all(buf).expect("vec writes should be infallible");
+    }
+    let (mut protocol, _) = writer.into_inner().expect("vec writes should be infallible");
+    (plaintext, protocol.derive_array())
+}
+
 proptest! {
     #![proptest_config(ProptestConfig::with_source_file("writers"))]
 
     #[test]
     fn mix_writer(writes in writes()) {
-        let mut slices = Protocol::new("com.example.test");
-        let combined = writes.iter().flatten().copied().collect::<Vec<u8>>();
-        slices.mix(&combined);
+        let tag = mix_as_slice(&writes);
+        let tag_p = mix_as_writer(&writes);
 
-        let writer = Protocol::new("com.example.test");
-        let mut writer = writer.mix_writer(io::sink());
-        for buf in writes {
-            writer.write_all(&buf).expect("sink writes should be infallible");
-        }
-        let (mut writer, _) = writer.into_inner();
-
-        prop_assert_eq!(slices.derive_array::<16>(), writer.derive_array::<16>(),
-            "two mixes of the same writes produced different outputs");
+        prop_assert_eq!(tag, tag_p);
     }
 
     #[test]
     fn encrypt_writer(writes in writes()) {
-        let mut slices = Protocol::new("com.example.test");
-        let mut ciphertext = writes.iter().flatten().copied().collect::<Vec<u8>>();
-        slices.encrypt(&mut ciphertext);
-
-        let writer = Protocol::new("com.example.test");
-        let mut ciphertext_p = Vec::new();
-        let mut writer = writer.encrypt_writer(&mut ciphertext_p);
-        for buf in writes.iter() {
-            writer.write_all(buf).expect("sink writes should be infallible");
-        }
-        let (mut writer, _) = writer.into_inner().expect("");
+        let (ciphertext, tag) = encrypt_as_slice(&writes);
+        let (ciphertext_p, tag_p) = encrypt_as_writer(&writes);
 
         prop_assert_eq!(ciphertext, ciphertext_p);
-        prop_assert_eq!(slices.derive_array::<16>(), writer.derive_array::<16>(),
-            "two encrypts of the same writes produced different outputs");
+        prop_assert_eq!(tag, tag_p);
     }
 
     #[test]
     fn decrypt_writer(writes in writes()) {
-        let mut slices = Protocol::new("com.example.test");
-        let mut plaintext = writes.iter().flatten().copied().collect::<Vec<u8>>();
-        slices.decrypt(&mut plaintext);
-
-        let writer = Protocol::new("com.example.test");
-        let mut plaintext_p = Vec::new();
-        let mut writer = writer.decrypt_writer(&mut plaintext_p);
-        for buf in writes.iter() {
-            writer.write_all(buf).expect("sink writes should be infallible");
-        }
-        let (mut writer, _) = writer.into_inner().expect("");
+        let (plaintext, tag) = decrypt_as_slice(&writes);
+        let (plaintext_p, tag_p) = decrypt_as_writer(&writes);
 
         prop_assert_eq!(plaintext, plaintext_p);
-        prop_assert_eq!(slices.derive_array::<16>(), writer.derive_array::<16>(),
-            "two decrypts of the same writes produced different outputs");
+        prop_assert_eq!(tag, tag_p);
     }
 }

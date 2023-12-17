62c53ee0da7f63ea506601261b6f8c5e622a046f
Coda Hale <coda.hale@gmail.com>
2023-12-17 15:03:02 -0700

    test(intrinsics): move intrinsic tests to the module

--- src/aegis_128l.rs
+++ src/aegis_128l.rs
@@ -248,54 +248,6 @@ mod tests {
     }
 
     #[test]
-    fn block_xor() {
-        let a = load(b"ayellowsubmarine");
-        let b = load(b"tuneintotheocho!");
-        let c = xor(a, b);
-
-        let mut c_bytes = [0u8; 16];
-        store(&mut c_bytes, c);
-
-        expect!["150c0b090501031c010a080e11010144"].assert_eq(&hex::encode(c_bytes));
-    }
-
-    #[test]
-    fn block_xor3() {
-        let a = load(b"ayellowsubmarine");
-        let b = load(b"tuneintotheocho!");
-        let c = load(b"mambonumbereight");
-        let d = xor3(a, b, c);
-
-        let mut d_bytes = [0u8; 16];
-        store(&mut d_bytes, d);
-
-        expect!["786d666b6a6f7671636f7a6b78666930"].assert_eq(&hex::encode(d_bytes));
-    }
-
-    #[test]
-    fn block_and() {
-        let a = load(b"ayellowsubmarine");
-        let b = load(b"tuneintotheocho!");
-        let c = and(a, b);
-
-        let mut c_bytes = [0u8; 16];
-        store(&mut c_bytes, c);
-
-        expect!["60716464686e74637460656162686e21"].assert_eq(&hex::encode(c_bytes));
-    }
-
-    #[test]
-    fn aes_round_test_vector() {
-        let a = load(&hex!("000102030405060708090a0b0c0d0e0f"));
-        let b = load(&hex!("101112131415161718191a1b1c1d1e1f"));
-        let out = enc(a, b);
-        let mut c = [0u8; 16];
-        store(&mut c, out);
-
-        expect!["7a7b4e5638782546a8c0477a3b813f43"].assert_eq(&hex::encode(c));
-    }
-
-    #[test]
     fn update_test_vector() {
         let mut state = Aegis128L {
             blocks: [
